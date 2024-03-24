b45396cb69f1ff1a9539c9d5306d4aa4220c16ff
may <git config --global user.email you@example.com>
2024-03-24 19:23:28 -0400

    removed random console logs

--- js/drag-and-drop.js
+++ js/drag-and-drop.js
@@ -4,13 +4,11 @@ export function init(){
         if (event.target.classList.contains('file-preview')){
             event.target.classList.add('dragging');
         }
-        console.log(event);
     });
 
     addEventListener("dragend", (event) => {
         if (event.target.classList.contains('file-preview')){
             event.target.classList.remove('dragging');
         }
-        console.log(event);
     });
 }
