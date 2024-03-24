18c166593d6e78058636bf318f2a1e05e4846009
may <git config --global user.email you@example.com>
2024-03-24 19:22:19 -0400

    added indicators for dragging

--- js/drag-and-drop.js
+++ js/drag-and-drop.js
@@ -1,76 +1,16 @@
-var current_dropzone = null;
-var dropzone_hover = false;
-
 export function init(){
-    class Draggable{
-        draggableArea;
-        mouseIsDown = false;
-        mouseIsHover = false;
 
-        constructor(draggableArea) {
-            const myself = this;
-            this.draggableArea = draggableArea;
-
-
-            this.draggableArea.addEventListener('mouseenter', function (e) {
-                // console.log("entered");
-                myself.isHover = true;
-            });
-            this.draggableArea.addEventListener('mouseout', function (e) {
-                // console.log("exited");
-                myself.isHover = false;
-            });
-
-            document.addEventListener('mousedown', function (e) {
-                e.preventDefault()
-                if (myself.isHover) {
-                    myself.isDown = true;
-                }
-            }, true);
-
-            document.addEventListener('mouseup', function (e) {
-                if (myself.isDown && dropzone_hover) {
-                    // console.log("dropping ", myself, "on ", current_dropzone);
-                    current_dropzone.drop(myself, e);
-                }
-                myself.isDown = false;
-            }, true);
+    addEventListener("dragstart", (event) => {
+        if (event.target.classList.contains('file-preview')){
+            event.target.classList.add('dragging');
         }
+        console.log(event);
+    });
 
-    }
-
-    class Dropzone{
-        dropArea;
-        mouseIsHover = false;
-        constructor(dropArea) {
-            const myself = this;
-            this.dropArea = dropArea;
-            this.dropArea.addEventListener('mouseenter', function (e) {
-                // console.log("entered");
-                myself.isHover = true;
-                current_dropzone = myself;
-                dropzone_hover = true;
-            });
-            this.dropArea.addEventListener('mouseout', function (e) {
-                // console.log("exited");
-                myself.isHover = false;
-                current_dropzone = null;
-                dropzone_hover = false
-            });
-            
+    addEventListener("dragend", (event) => {
+        if (event.target.classList.contains('file-preview')){
+            event.target.classList.remove('dragging');
         }
-        drop(draggableArea, mouseEvent) {
-            console.log("item: ", draggableArea);
-            console.log(" dropped on ", this);
-            console.log(" event: ", mouseEvent)
-        }
-    }
-
-    for (const draggableArea of document.querySelectorAll('.file-preview')){
-        new Draggable(draggableArea);
-    }
-
-    for (const dropArea of document.querySelectorAll('.timeline_container')){
-        new Dropzone(dropArea);
-    }
+        console.log(event);
+    });
 }
